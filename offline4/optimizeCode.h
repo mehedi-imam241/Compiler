fba3fa5c580894219f9213cd2f6390315dfc5dd9
mehedi-imam241 <1805039@ugrad.cse.buet.ac.bd>
2022-09-05 00:05:14 +0600

    final_version

--- offline4/optimizeCode.h
+++
@@ -1,7 +0,0 @@
-#ifndef OPTIMIZECODE_H
-
-#include <bits/stdc++.h>
-using namespace std;
-void optimizeCode();
-
-#endif // !OPTIMIZECODE_H
--- offline4/script.sh
+++
@@ -1,7 +0,0 @@
-rm log.txt error.txt AssemblyCode.asm OptimizedAssemblyCode.asm
-bison -d 1805039.y
-flex 1805039.l
-g++ lex.yy.c 1805039.tab.c 
-./a.out $1 log.txt error.txt AssemblyCode.asm OptimizedAssemblyCode.asm
-rm 1805039.tab.c 1805039.tab.h lex.yy.c
-
